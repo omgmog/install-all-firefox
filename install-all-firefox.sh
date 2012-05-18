dcbf37fb1c7fa59f5ebfc3f53b48ea436f84a92f
Max Glenister <moglenstar@gmail.com>
2012-05-18 09:15:58 +0100

    Updated Firefox 3.5.9 > 3.5.19 in default versions

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 default_versions_future="beta aurora nightly ux"
 default_versions_current="12.0"
-default_versions_past="2.0.0.20 3.0.19 3.5.9 3.6.28 4.0.1 5.0.1 6.0.2 7.0.1 8.0.1 9.0.1 10.0.2 11.0"
+default_versions_past="2.0.0.20 3.0.19 3.5.19 3.6.28 4.0.1 5.0.1 6.0.2 7.0.1 8.0.1 9.0.1 10.0.2 11.0"
 
 default_versions="${default_versions_past} ${default_versions_current} ${default_versions_future}"
 tmp_directory="/tmp/firefoxes/"

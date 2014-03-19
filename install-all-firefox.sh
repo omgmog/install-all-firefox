fc5f7bb0c1c7c827eb40af263c9a3b1acae172a3
Max Glenister <moglenstar@gmail.com>
2014-03-19 09:06:54 +0000

    Added Firefox 28.0

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 default_versions_future="beta aurora nightly ux"
-default_versions_current="27"
-default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26"
+default_versions_current="28"
+default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27"
 
 versions_usage_point_one="10 11 13 14 18"
 versions_usage_point_two="12 15 17 19"
@@ -438,6 +438,20 @@ get_associated_information(){
       binary="firefox"
       short_name="fx27"
       nice_name="Firefox 27.0"
+
+      firebug_version="1.12.0"
+      firebug_version_short=`echo "${firebug_version}" | sed 's/\.[0-9a-zA-Z]*$//'`
+      firebug_root="http://getfirebug.com/releases/firebug/${firebug_version_short}/"
+      firebug_file="firebug-${firebug_version}.xpi"
+      ;;
+    28 | 28.0)
+      ftp_root="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/28.0/"
+      dmg_file="Firefox 28.0.dmg"
+      sum_file="MD5SUMS"
+      sum_file_type="md5"
+      binary="firefox"
+      short_name="fx28"
+      nice_name="Firefox 28.0"
 
       firebug_version="1.12.0"
       firebug_version_short=`echo "${firebug_version}" | sed 's/\.[0-9a-zA-Z]*$//'`

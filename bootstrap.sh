f0b065621bc93d156be4ad38cbf4b69f0bfc3ec5
Max Glenister <moglenstar@gmail.com>
2014-05-29 19:26:29 +0100

    Rewrote bootstrap.sh to be less spaghetti-like

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,37 +1,46 @@
 #!/bin/bash
-# This is just here for the sake of people running bootstrap.sh with curl still
 
-log(){
-    printf "\n\033[32m$*\033[00m\n"
-    return $?
+readonly PROGNAME=$(basename $0)
+
+function main {
+    local remote_dir="https://raw.github.com/omgmog/install-all-firefox/master/"
+    local script_name="firefoxes.sh"
+    local temp_dir="/tmp/"
+    local remote_script="${remote_dir}${script_name}"
+    local local_script="${temp_dir}${script_name}"
+    local choice_made=0
+    local current_dir="${PWD}/"
+
+    echo -e "You should be using ${script_name} not ${PROGNAME}."
+    echo -e "I'll download the new script for you now."
+
+    if curl -L "${remote_script}" -o "${local_script}"; then
+
+        echo -e "Successfully downloaded ${script_name} to ${local_script}"
+
+        chmod +x "${local_script}"
+
+        echo -e "Do you want to copy ${local_script} to your current directory?"
+        read user_choice
+        while [[ "${choice_made}" < 1 ]]; do
+
+            case "$(echo ${user_choice} | tr '[:upper:]' '[:lower:]')" in
+                "y" | "yes")
+                    choice_made=1
+                    echo -e "Copying ${script_name} to ${current_dir}"
+                    cp "${local_script}" "${current_dir}${script_name}"
+                ;;
+                "n" | "no")
+                    choice_made=1
+                    echo -e "Okay, I'll leave it in ${local_script}"
+                ;;
+                *)
+                    echo -e "Please enter 'y' or 'n'."
+                    read user_choice
+                ;;
+            esac
+        done
+    fi
 }
 
-echo "The script now uses 'firefoxes.sh' not 'bootstrap.sh', I'll go ahead and download the updated script for you now..."
-
-if curl -C - -L "https://raw.github.com/omgmog/install-all-firefox/master/firefoxes.sh" -o "/tmp/firefoxes.sh"
-	then
-	chmod +x "/tmp/firefoxes.sh"
-	log "✔ Successfully downloaded the new 'firefoxes.sh'!"
-	echo "Would you like to copy 'firefoxes.sh' from /tmp/ to your current directory (${PWD})? [y/n]"
-	read user_choice
-	choice_made="false"
-	while [[ "$choice_made" == "false" ]]
-	do
-		case "$user_choice" in
-			"y")
-				choice_made="true"
-				log "Copying 'firefoxes.sh' to ${PWD}/firefoxes.sh"
-				cp "/tmp/firefoxes.sh" "${PWD}/firefoxes.sh"
-			;;
-			"n")
-				choice_made="true"
-				echo "Okay then, you will need to move the file manually. It's currently stored in /tmp/firefoxes.sh"
-			;;
-			*)
-				choice_made="true"
-				echo "Please enter 'y' or 'n'"
-				read user_choice
-			;;
-		esac
-	done
-fi
+main

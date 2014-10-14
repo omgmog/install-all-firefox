26b3bb2d4fcb59a39947d2078eaeba61b2a55667
Max Glenister <moglenstar@gmail.com>
2014-10-14 17:16:53 +0100

    Added Firefox 33

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 default_versions_future="beta aurora nightly ux"
-default_versions_current="32"
-default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31"
+default_versions_current="33"
+default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32"
 
 versions_usage_point_one="3.6 12 16 17 22 23 25"
 versions_usage_point_two="21 24 26"
@@ -512,6 +512,20 @@ get_associated_information(){
       binary="firefox"
       short_name="fx32"
       nice_name="Firefox 32.0"
+
+      firebug_version="2.0"
+      firebug_version_short=$firebug_version
+      firebug_root="http://getfirebug.com/releases/firebug/${firebug_version_short}/"
+      firebug_file="firebug-${firebug_version}.xpi"
+      ;;
+    33 | 33.0)
+      ftp_root="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/33.0/"
+      dmg_file="Firefox 33.0.dmg"
+      sum_file="MD5SUMS"
+      sum_file_type="md5"
+      binary="firefox"
+      short_name="fx33"
+      nice_name="Firefox 33.0"
 
       firebug_version="2.0"
       firebug_version_short=$firebug_version

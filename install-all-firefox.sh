487c145eb36961c47ca63ea9a66fec8636d0710a
Max Glenister <moglenstar@gmail.com>
2014-12-07 09:47:30 +0000

    Fixed incorrect "34" > "34.0"

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -557,8 +557,8 @@ get_associated_information(){
             firebug_file="firebug-${firebug_version}.xpi"
         ;;
         34)
-            ftp_root="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/34/"
-            dmg_file="Firefox 34.dmg"
+            ftp_root="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/34.0/"
+            dmg_file="Firefox 34.0.dmg"
             sum_file="MD5SUMS"
             sum_file_type="md5"
             binary="firefox"

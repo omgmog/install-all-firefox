585b7638f8e861bd9e52881f48578076a750058f
jdalton <john.david.dalton@gmail.com>
2015-05-18 21:18:52 -0700

    Add fx38.

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 default_versions_future="beta aurora nightly ux"
-default_versions_current="37"
-default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36"
+default_versions_current="38"
+default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37"
 
 versions_usage_point_one="3.6 12 16 17 22 23 25"
 versions_usage_point_two="21 24 26"
@@ -617,6 +617,22 @@ get_associated_information(){
             uses_v2_signing=true
 
             firebug_version="2.0.8"
+            firebug_version_short=$(echo "${firebug_version}" | sed 's/\.[0-9a-zA-Z]*$//')
+            firebug_root="http://getfirebug.com/releases/firebug/${firebug_version_short}/"
+            firebug_file="firebug-${firebug_version}.xpi"
+        ;;
+        38 | 38.0)
+            ftp_root="ftp://ftp.mozilla.org/pub/mozilla.org/firefox/releases/38.0/"
+            dmg_file="Firefox 38.0.dmg"
+            sum_file="MD5SUMS"
+            sum_file_type="md5"
+            binary="firefox"
+            short_name="fx37"
+            nice_name="Firefox 38.0"
+
+            uses_v2_signing=true
+
+            firebug_version="2.0.9"
             firebug_version_short=$(echo "${firebug_version}" | sed 's/\.[0-9a-zA-Z]*$//')
             firebug_root="http://getfirebug.com/releases/firebug/${firebug_version_short}/"
             firebug_file="firebug-${firebug_version}.xpi"

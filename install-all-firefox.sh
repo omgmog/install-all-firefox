a6bf93b23e66af1901fa85da1abe6672c8d2ca17
John-David Dalton <john.david.dalton@gmail.com>
2015-12-18 23:50:34 -0600

    Add fx43.

--- install-all-firefox.sh
+++ install-all-firefox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-default_versions_current="42"
-default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41"
+default_versions_current="43"
+default_versions_past="2 3 3.5 3.6 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42"
 
 versions_usage_point_one="3.6 12 16 17 22 23 25"
 versions_usage_point_two="21 24 26"
@@ -692,6 +692,22 @@ get_associated_information(){
             binary="firefox"
             short_name="fx42"
             nice_name="Firefox 42.0"
+
+            uses_v2_signing=true
+
+            firebug_version="2.0.13"
+            firebug_version_short=$(echo "${firebug_version}" | sed 's/\.[0-9a-zA-Z]*$//')
+            firebug_root="http://getfirebug.com/releases/firebug/${firebug_version_short}/"
+            firebug_file="firebug-${firebug_version}.xpi"
+        ;;
+        43 | 43.0)
+            release_directory="43.0"
+            dmg_file="Firefox 43.0.dmg"
+            sum_file="MD5SUMS"
+            sum_file_type="md5"
+            binary="firefox"
+            short_name="fx43"
+            nice_name="Firefox 43.0"
 
             uses_v2_signing=true
 
